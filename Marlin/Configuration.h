5f4f241af96eb8b9b1dd4056b455205af3b29083
Jean-Pierre Thomasset <jpthomasset@gmail.com>
2018-03-31 17:34:54 +0200

    Change Extruder configuration for new mk8 extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -529,7 +529,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100.2, 100, 400, 156.250 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100.2, 100, 400, 87.29 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -758,7 +758,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
